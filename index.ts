6a251727275b46ba5533e076ddded09340fc7035
Harminder Virk <virk.officials@gmail.com>
2023-12-24 12:40:04 +0530

    chore: setup bundling

---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ * We need this empty file so that tsup bundling retains
+ * the directory structure for rest of the files inside
+ * the "src" directory.
+ *
+ * Otherwise, it outputs the src folder contents one level
+ * up.
+ *
+ * Yeah, one of those weird outcomes of using bundlers.
+ */
